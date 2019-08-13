3722ab241375fc69f943522caf91aff0e484b609
yongxinyou <1014687086@qq.com>
2019-08-13 22:59:27 +0800

    后端渲染 - render

--- vote/views.py
+++ vote/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 sayings = [
     '真理惟一可靠的标准就是永远自相符合',
@@ -13,13 +14,11 @@ all_fruits = ['西瓜', '苹果', '香蕉', '榴莲', '梨子', '葡萄'
               '山竹', '橘子', '火龙果', '荔枝', '草莓', '蓝莓']
 
 
+# 后端渲染 - 服务器把动态内容渲染到静态的HTML页面上在输出给浏览器
+# return render(request, 'index.html', locals())
+# 前端渲染 - 服务器只给JSON数据不负责渲染页面，
+# 渲染页面的工作交给浏览器的JavaScript来完成(Vue+jQuery) -- 趋势
 def home(request):
-    sentence = sayings[random.randrange(len(sayings))]
-    content = f'<h1>{sentence}</h1>'
-    content += '<hr>'
-    content += '<h2>今天向你推荐的水果</h2>'
-    content += '<ul>'
-    for fruit in random.sample(all_fruits, 3):
-        content += f'<li>{fruit}</li>'
-    content += '</ul>'
-    return HttpResponse(content)
+    context = {'sentence': sayings[random.randrange(len(sayings))],
+               'fruits': random.sample(all_fruits, 3)}
+    return render(request, 'home.html', context=context)
